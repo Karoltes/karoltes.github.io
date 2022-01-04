d898115071dec0998f31de4ac568a24add16ddc2
Karoltes <karolbr@spoko.pl>
2022-01-04 10:55:26 +0100

    new page

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function changeCont(i){
-    document.getElementsByTagName("div")[0].style.display = "none"; 
-    document.getElementsByTagName("div")[1].style.display = "none";
-    document.getElementsByTagName("div")[2].style.display = "none";
-    if (i == 1) document.getElementsByTagName("div")[0].style.display = "inline-block"; 
-    else if (i == 2) document.getElementsByTagName("div")[1].style.display = "inline-block";
-    else if (i == 3) document.getElementsByTagName("div")[2].style.display = "inline-block";
-}
