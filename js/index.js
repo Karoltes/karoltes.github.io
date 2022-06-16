54c793e712c2fd6cedc60f6bd614e69743225d6a
Karoltes <karolbr@spoko.pl>
2022-06-16 18:43:16 +0200

    cad page

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
-function changeContent(i){
-    document.querySelectorAll(".content")[0].style.display = "none"; 
-    document.querySelectorAll(".content")[1].style.display = "none";
-    document.querySelectorAll(".content")[2].style.display = "none";
-    if (i === 1) document.querySelectorAll(".content")[0].style.display = "inline-block"; 
-    else if (i === 2) document.querySelectorAll(".content")[1].style.display = "inline-block";
-    else if (i === 3) document.querySelectorAll(".content")[2].style.display = "inline-block";
+
+function showMore(el){
+    let details = el.previousElementSibling;
+    details.style.display = 'block';
+    el.style.display = 'none';
 }
